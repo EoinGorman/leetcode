bce70ab16b1fb50ba6f259c07a0b7739b5cc2697
Eoin Gorman <eoingorman@live.ie>
2022-06-08 11:17:35 +0200

    876 first solution

--- 876_middle_of_the_linked_list/solution.rb
+++ 876_middle_of_the_linked_list/solution.rb
@@ -10,16 +10,27 @@ end
 # @param {ListNode} head
 # @return {ListNode}
 def middle_node(head)
+  middle = head
+  node_count = 0
+  until head.nil?
+    head = head.next
+    node_count += 1
+  end
 
+  middle_number = (node_count / 2)
+  middle_number.times do
+    middle = middle.next
+  end
+  middle
 end
 
 if __FILE__ == $0
   n5 = ListNode.new(5, nil)
-  n4 = ListNode.new(5, n5)
-  n3 = ListNode.new(5, n4)
-  n2 = ListNode.new(5, n3)
-  n1 = ListNode.new(5, n2)
+  n4 = ListNode.new(4, n5)
+  n3 = ListNode.new(3, n4)
+  n2 = ListNode.new(2, n3)
+  n1 = ListNode.new(1, n2)
 
   result = middle_node(n1)
-  puts "Result: #{result}"
+  puts "Result: #{result}: #{result.val}"
 end
